ae5e45b6ff72f80d953e3dbe1aa9842351505b16
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-02-11 19:17:36 +0000

    version beta

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const pronouns = ["the", "our"];
+const adjectives = ["great", "big", "super"];
+const nouns = ["jogger", "racoon", "coders"];
+const extensions = [".com", ".es", ".net"];
+const btn = document.getElementById("createDomainNames");
+
+btn.addEventListener("click", getDomainNames);
+
+function getDomainNames() {
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let div = document.createElement("div");
+          div.innerText =
+            pronouns[i] + adjectives[j] + nouns[k] + extensions[l];
+          document.body.appendChild(div);
+        }
+      }
+    }
+  }
+}
