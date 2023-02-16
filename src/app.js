1758bb71b5eda031d89161b8bee9202b7a2a1477
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-02-16 12:04:33 +0000

    first refactorization

--- src/app.js
+++ src/app.js
@@ -27,26 +27,31 @@ const btn = document.getElementById("createDomainNames");
 const div = document.getElementById("domains");
 btn.addEventListener("click", all);
 
+function getValue(idInput) {
+  return document.getElementById(idInput).value;
+}
+
 function getPronouns() {
-  const pronounOne = document.getElementById("pronoun1").value;
-  const pronounTwo = document.getElementById("pronoun2").value;
+  const pronounOne = getValue("pronoun1");
+  const pronounTwo = getValue("pronoun2");
   pronouns.push(pronounOne, pronounTwo);
   console.log(pronouns);
 }
 
 function getAdjectives() {
-  const adjectiveOne = document.getElementById("adjective1").value;
-  const adjectiveTwo = document.getElementById("adjective2").value;
+  const adjectiveOne = getValue("adjective1");
+  const adjectiveTwo = getValue("adjective2");
   adjectives.push(adjectiveOne, adjectiveTwo);
   console.log(adjectives);
 }
 
 function getNouns() {
-  const nameOne = document.getElementById("name1").value;
-  const nameTwo = document.getElementById("name2").value;
+  const nameOne = getValue("name1");
+  const nameTwo = getValue("name2");
   nouns.push(nameOne, nameTwo);
   console.log(nouns);
 }
+
 function setDomainNames() {
   getPronouns();
   getAdjectives();
@@ -67,20 +72,7 @@ function setDomainNames() {
     });
   });
 }
+
 function all() {
   setDomainNames();
 }
-// function getDomainNames() {
-//   for (let i = 0; i < pronouns.length; i++) {
-//     for (let j = 0; j < adjectives.length; j++) {
-//       for (let k = 0; k < nouns.length; k++) {
-//         for (let l = 0; l < extensions.length; l++) {
-//           let divOne = document.createElement("div");
-//           divOne.innerText =
-//             pronouns[i] + adjectives[j] + nouns[k] + extensions[l];
-//           div.appendChild(divOne);
-//         }
-//       }
-//     }
-//   }
-// }
