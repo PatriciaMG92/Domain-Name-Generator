dbc7441b18ee7f132992198a0b3877c536dbf14d
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-02-16 11:55:50 +0000

    add personalization

--- src/app.js
+++ src/app.js
@@ -16,18 +16,41 @@ import "./assets/img/4geeks.ico";
 
 //usar for.each y .map juntos??
 
-const pronouns = ["the", "our"];
-const adjectives = ["great", "big", "super"];
-const nouns = ["jogger", "panda", "racoon"];
+const pronouns = [];
+const adjectives = [];
+const nouns = [];
 const extensions = [".com", ".es", ".net"];
 
 const result = [];
 
 const btn = document.getElementById("createDomainNames");
 const div = document.getElementById("domains");
-btn.addEventListener("click", setDomainNames);
+btn.addEventListener("click", all);
 
+function getPronouns() {
+  const pronounOne = document.getElementById("pronoun1").value;
+  const pronounTwo = document.getElementById("pronoun2").value;
+  pronouns.push(pronounOne, pronounTwo);
+  console.log(pronouns);
+}
+
+function getAdjectives() {
+  const adjectiveOne = document.getElementById("adjective1").value;
+  const adjectiveTwo = document.getElementById("adjective2").value;
+  adjectives.push(adjectiveOne, adjectiveTwo);
+  console.log(adjectives);
+}
+
+function getNouns() {
+  const nameOne = document.getElementById("name1").value;
+  const nameTwo = document.getElementById("name2").value;
+  nouns.push(nameOne, nameTwo);
+  console.log(nouns);
+}
 function setDomainNames() {
+  getPronouns();
+  getAdjectives();
+  getNouns();
   const generateDomainNames = pronouns.map(function(valueOfPronoun) {
     adjectives.map(function(valueofAdjective) {
       nouns.map(function(valueOfNoun) {
@@ -44,7 +67,9 @@ function setDomainNames() {
     });
   });
 }
-
+function all() {
+  setDomainNames();
+}
 // function getDomainNames() {
 //   for (let i = 0; i < pronouns.length; i++) {
 //     for (let j = 0; j < adjectives.length; j++) {
